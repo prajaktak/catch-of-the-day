4d7c4f3ee16539d7e771d5c7e4a623ae2e9f0fa5
Prajakta Kulkarni <prachee.j@gmnail.com>
2018-08-21 16:34:35 +0200

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Header = props =>
+//const Header = ({tagline}) =>
+//const Header = (props) => {
+//function Header (props){
+    //return(
+        (
+        < header className="top">
+            <h1>
+            Catch 
+            <span className="ofThe">
+                <span className="of">of</span>
+                <span className="the">the</span>
+            </span>
+            day
+            </h1>
+            <h3 className="tagline">
+                <span>{props.tagline}</span>
+            </h3> 
+        </ header>
+    );
+//}
+
+// class Header extends React.Component {
+//     render(){
+//         return (
+//             < header className="top">
+//                 <h1>
+//                 Catch 
+//                 <span className="ofThe">
+//                     <span className="of">of</span>
+//                     <span className="the">the</span>
+//                 </span>
+//                 day
+//                 </h1>
+//                 <h3 className="tagline">
+//                 <span>{this.props.tagline}</span>
+//             </h3> 
+//             </ header>
+//         )
+//     }
+// }
+export default Header;
---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import { format } from 'url';
+
+class StorePicker extends React.Component{
+
+    render(){
+        return 
+    }
+}
+
+export default StorePicker;
