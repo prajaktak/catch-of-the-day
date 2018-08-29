66b05788a89d0e18df5864d9bc032c9934f8dcd6
Prajakta Kulkarni <prachee.j@gmnail.com>
2018-08-29 18:20:26 +0200

    changes till lecture 14

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,27 @@ import Header from './Header'
 import Inventory from './Inventory'
 import Order from './Order'
 
-class App extends React.Component {
-    render() {
+class App extends React.Component{
+    state = {
+        fishes:{},
+        order:{}
+    };
+    addFish = (fish) => {
+        //1. Take a copy of the existing state
+        const fishes = {...this.state.fishes} // object spread 
+        //2. Add new Fish to fishes object
+        fishes[`fish${Date.now()}`] = fish;
+        //3. set the new fishes object new fishes
+        this.setState({ fishes }); 
+    };
+    render(){
         return (
             <div className="catch-of-the-day">
                 <div className = "menu">
                     <Header tagline="Fresh seafood market"/>
                 </div>
                 <Order/>
-                <Inventory/>
+                <Inventory addFish={this.addFish} />
             </div>
         )
     }
